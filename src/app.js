1d6f58ca396ad414847e5c5212440ca4aeb940fb
Marcel Rebmann <marcelrebmann@me.com>
2017-06-02 11:10:28 +0200

    Adds scripts

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-/**
- * Created by marce on 02.06.2017.
- */
+var rpio = require('rpio');
+
+function pollcb(pin)
+{
+    /*
+     * Interrupts aren't supported by the underlying hardware, so events
+     * may be missed during the 1ms poll window.  The best we can do is to
+     * print the current state after a event is detected.
+     */
+    var state = rpio.read(pin) ? 'pressed' : 'released';
+    console.log('Button event on P%d (button currently %s)', pin, state);
+}
+
+rpio.poll(8, pollcb);
